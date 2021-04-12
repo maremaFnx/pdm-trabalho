cb79fa83a89b92d68eb09b6977dc00628485ad2a
Alesson Junior Toniazzo <alesson.j@aluno.ifsc.edu.br>
2021-04-12 09:11:37 -0300

    Att

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MyComponent from './components/teste';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MyComponent></MyComponent>
+          
     </View>
   );
 }
@@ -14,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
