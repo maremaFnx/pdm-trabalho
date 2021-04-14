dfe11ed090288f4519695fed7a694af8fe3b350d
Alesson Junior Toniazzo <alesson.j@aluno.ifsc.edu.br>
2021-04-13 21:43:28 -0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-          
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
